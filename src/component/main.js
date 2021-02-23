408f1da9f6dffe767d83c0c7d2a4f5c5e77b511c
enlighten96gm <gm.enlightenby@gmail.com>
2021-02-23 14:51:08 +0300

    first fix

--- src/component/main.js
+++ src/component/main.js
@@ -1,6 +1,19 @@
+import s from "./main.module.css"
 const Main = () => {
     return (
-        <div>Privet</div>
+        <div className={s.body}>
+            <div className={s.container}>
+                <div data-text="CHARACKET" className={s.beforeText}>
+                    <div className={s.text}>character</div>
+                </div>
+                <div data-text="SKILLS" className={s.beforeText}>
+                    <div className={s.text}>SKILLS </div>
+                </div>
+                <div data-text="ANOUTHER" className={s.beforeText}>
+                    <div className={s.text}>anouther</div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Main;
