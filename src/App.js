1a58fd51864033814834170d1155c5ff55f39d10
enlighten96gm <gm.enlightenby@gmail.com>
2021-02-26 17:59:01 +0300

    links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Main from "./component/main"
+import Skills from './component/skills/skills';
+import Character from './component/character/character'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Main />
+    <BrowserRouter>
+    <div>
+      <div>
+        <Main />
+        <Route path='/skills' component={Skills}/>
+        <Route path='/character' component={Character}/>
+      </div>
+    </div>
+    </BrowserRouter>
+      
   )
 }
 
--- src/component/main.js
+++ src/component/main.js
@@ -1,16 +1,24 @@
+import { NavLink } from "react-router-dom"
+import Charcter from "./character/character"
 import s from "./main.module.css"
+
+
 const Main = () => {
     return (
         <div className={s.body}>
             <div className={s.container}>
-                <div data-text="CHARACKET" className={s.beforeText}>
-                    <div className={s.text}>character</div>
+                <div data-text={"CHARACTER"} className={s.beforeText}>
+                    <div className={s.text}>
+                        <NavLink className={s.link} to='/character'>character</NavLink>
+                    </div>
                 </div>
                 <div data-text="SKILLS" className={s.beforeText}>
-                    <div className={s.text}>SKILLS </div>
+                    <div className={s.text}>
+                        <NavLink className={s.link} to='/skills'>skills</NavLink>
+                    </div>
                 </div>
-                <div data-text="ANOUTHER" className={s.beforeText}>
-                    <div className={s.text}>anouther</div>
+                <div data-text="ANOTHER" className={s.beforeText}>
+                    <div className={s.text}>another</div>
                 </div>
             </div>
         </div>
