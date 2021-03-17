6b5b28b2640861aae522dcd9ab07bfe0c682224d
enlighten96gm <gm.enlightenby@gmail.com>
2021-03-17 14:25:38 +0300

    blocks

--- src/component/skills/skills.js
+++ src/component/skills/skills.js
@@ -2,7 +2,18 @@ import s from "./skills.module.css"
 const Skills = () => {
     return (
         <div className={s.smt}>
-            <div className={s.main}>Skills</div>
+            <div className={s.main}>
+                <div className={s.java}>
+                    <div className={s.sqare1}></div>
+                    <div className={s.sqare2}></div>
+                </div>
+                <div className={s.react}>
+                    <div className={s.circle1}></div>
+                    <div className={s.circle2}></div>
+                </div>
+                <div className={s.typescript}></div>
+                
+            </div>
         </div>
     )
 }
